f8ca613161baa2c1203f97b6048837e3cf6c065a
xiaolf <xiaolf@seagroup.com>
2021-04-20 17:25:42 +0800

    updated

--- example/src/main/java/SlidingWindowJoin.java
+++ example/src/main/java/SlidingWindowJoin.java
@@ -1,6 +1,7 @@
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -13,23 +14,23 @@ public class SlidingWindowJoin {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        DataStream<Tuple2<Integer, Long>> orangeStream = env.fromElements(
-                Tuple2.of(0, 0L), // integer, timestamp
-                Tuple2.of(1, 1L),
-                Tuple2.of(2, 2L),
-                Tuple2.of(3, 3L),
-                Tuple2.of(4, 4L))
+        DataStream<Tuple3<Integer, Integer, Long>> orangeStream = env.fromElements(
+                Tuple3.of(0, 0, 0L), // key, value, timestamp
+                Tuple3.of(0, 1, 1L),
+                Tuple3.of(0, 2, 2L),
+                Tuple3.of(0, 3, 3L),
+                Tuple3.of(0, 4, 4L))
                 .assignTimestampsAndWatermarks(
                         WatermarkStrategy
-                                .<Tuple2<Integer, Long>>forBoundedOutOfOrderness(Duration.ofMillis(0))
+                                .<Tuple3<Integer, Integer, Long>>forBoundedOutOfOrderness(Duration.ofMillis(0))
                                 .withTimestampAssigner((event, timestamp) -> event.f1));
-        DataStream<Tuple2<Integer, Long>> greenStream = env.fromElements(
-                Tuple2.of(0, 0L),
-                Tuple2.of(3, 3L),
-                Tuple2.of(4, 4L))
+        DataStream<Tuple3<Integer, Integer, Long>> greenStream = env.fromElements(
+                Tuple3.of(0, 0, 0L),
+                Tuple3.of(0, 3, 3L),
+                Tuple3.of(0, 4, 4L))
                 .assignTimestampsAndWatermarks(
                         WatermarkStrategy
-                                .<Tuple2<Integer, Long>>forBoundedOutOfOrderness(Duration.ofMillis(0))
+                                .<Tuple3<Integer, Integer, Long>>forBoundedOutOfOrderness(Duration.ofMillis(0))
                                 .withTimestampAssigner((event, timestamp) -> event.f1));
 
         orangeStream.join(greenStream)
@@ -42,19 +43,23 @@ public class SlidingWindowJoin {
         env.execute();
     }
 
-    public static class IntegerJoinFunction implements JoinFunction<Tuple2<Integer, Long>, Tuple2<Integer, Long>, String> {
+    public static class IntegerJoinFunction implements JoinFunction<Tuple3<Integer, Integer, Long>, Tuple3<Integer, Integer, Long>, String> {
         @Override
-        public String join(Tuple2<Integer, Long> first, Tuple2<Integer, Long> second) {
+        public String join(Tuple3<Integer, Integer, Long> first, Tuple3<Integer, Integer, Long> second) {
             return first + ", " + second;
         }
     }
 }
 
 /*
-6> (0,0), (0,0)
-8> (3,3), (3,3)
-1> (4,4), (4,4)
-6> (0,0), (0,0)
-1> (4,4), (4,4)
-8> (3,3), (3,3)
+6> (0,0,0), (0,0,0)
+6> (0,0,0), (0,0,0)
+6> (0,1,1), (0,0,0)
+6> (0,2,2), (0,3,3)
+6> (0,3,3), (0,3,3)
+6> (0,3,3), (0,3,3)
+6> (0,3,3), (0,4,4)
+6> (0,4,4), (0,3,3)
+6> (0,4,4), (0,4,4)
+6> (0,4,4), (0,4,4)
  */
--- example/src/main/java/TumblingWindowJoin.java
+++ example/src/main/java/TumblingWindowJoin.java
@@ -16,27 +16,27 @@ public class TumblingWindowJoin {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        DataStream<Tuple2<Integer, Long>> orangeStream = env.fromElements(
-                Tuple2.of(0, 0L), // integer, timestamp
-                Tuple2.of(1, 1L),
-                Tuple2.of(2, 2L),
-                Tuple2.of(3, 3L),
-                Tuple2.of(4, 4L),
-                Tuple2.of(5, 5L),
-                Tuple2.of(6, 6L),
-                Tuple2.of(7, 7L))
+        DataStream<Tuple3<Integer, Integer, Long>> orangeStream = env.fromElements(
+                Tuple3.of(0, 0, 0L), // key, value, timestamp
+                Tuple3.of(0, 1, 1L),
+                Tuple3.of(0, 2, 2L),
+                Tuple3.of(0, 3, 3L),
+                Tuple3.of(0, 4, 4L),
+                Tuple3.of(0, 5, 5L),
+                Tuple3.of(0, 6, 6L),
+                Tuple3.of(0, 7, 7L))
                 .assignTimestampsAndWatermarks(
                         WatermarkStrategy
-                                .<Tuple2<Integer, Long>>forBoundedOutOfOrderness(Duration.ofMillis(0))
+                                .<Tuple3<Integer, Integer, Long>>forBoundedOutOfOrderness(Duration.ofMillis(0))
                                 .withTimestampAssigner((event, timestamp) -> event.f1));
-        DataStream<Tuple2<Integer, Long>> greenStream = env.fromElements(
-                Tuple2.of(0, 0L),
-                Tuple2.of(1, 1L),
-                Tuple2.of(3, 3L),
-                Tuple2.of(4, 4L))
+        DataStream<Tuple3<Integer, Integer, Long>> greenStream = env.fromElements(
+                Tuple3.of(0, 0, 0L),
+                Tuple3.of(0, 1, 1L),
+                Tuple3.of(0, 3, 3L),
+                Tuple3.of(0, 4, 4L))
                 .assignTimestampsAndWatermarks(
                         WatermarkStrategy
-                                .<Tuple2<Integer, Long>>forBoundedOutOfOrderness(Duration.ofMillis(0))
+                                .<Tuple3<Integer, Integer, Long>>forBoundedOutOfOrderness(Duration.ofMillis(0))
                         .withTimestampAssigner((event, timestamp) -> event.f1));
         orangeStream.join(greenStream)
                 .where(x -> x.f0)
@@ -49,17 +49,21 @@ public class TumblingWindowJoin {
 
     }
 
-    public static class IntegerJoinFunction implements JoinFunction<Tuple2<Integer, Long>, Tuple2<Integer, Long>, String> {
+    public static class IntegerJoinFunction implements JoinFunction<Tuple3<Integer, Integer, Long>, Tuple3<Integer, Integer, Long>, String> {
         @Override
-        public String join(Tuple2<Integer, Long> first, Tuple2<Integer, Long> second) {
+        public String join(Tuple3<Integer, Integer, Long> first, Tuple3<Integer, Integer, Long> second) {
             return first + ", " + second;
         }
     }
 }
 
 /*
-1> (4,4), (4,4)
-6> (0,0), (0,0)
-8> (3,3), (3,3)
-6> (1,1), (1,1)
+6> (0,0,0), (0,0,0)
+6> (0,0,0), (0,1,1)
+6> (0,1,1), (0,0,0)
+6> (0,1,1), (0,1,1)
+6> (0,2,2), (0,3,3)
+6> (0,3,3), (0,3,3)
+6> (0,4,4), (0,4,4)
+6> (0,5,5), (0,4,4)
  */
