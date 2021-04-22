80be4437d37af23ba03a295093098821539f358b
largecats <linfanxiaolinda@outlook.com>
2021-04-23 01:12:42 +0800

    updated

--- examples/src/main/java/processFunction/CountWithTimeoutBatch.java
+++ examples/src/main/java/processFunction/CountWithTimeoutBatch.java
@@ -20,20 +20,22 @@ public class CountWithTimeoutBatch {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        DataStream<Tuple3<String, String, Long>> stream = env.fromElements( // how to simulate timeout?
-                Tuple3.of("a", "abase", 0L),
-                Tuple3.of("b", "bard", 0L),
-                Tuple3.of("a", "abate", 70000L),
-                Tuple3.of("b", "barrage", 70000L),
-                Tuple3.of("a", "abbreviate", 140000L),
-                Tuple3.of("b", "baroque", 140000L),
-                Tuple3.of("a", "abdicate", 210000L),
-                Tuple3.of("b", "barren", 210000L)
-                )
-                .assignTimestampsAndWatermarks(
-                        WatermarkStrategy
-                                .<Tuple3<String, String, Long>>forBoundedOutOfOrderness(Duration.ofMillis(0))
-                                .withTimestampAssigner((event, timestamp) -> event.f2));
+//        DataStream<Tuple3<String, String, Long>> stream = env.fromElements( // can't simulate timeout
+//                Tuple3.of("a", "abase", 0L),
+//                Tuple3.of("b", "bard", 0L),
+//                Tuple3.of("a", "abate", 70000L),
+//                Tuple3.of("b", "barrage", 70000L),
+//                Tuple3.of("a", "abbreviate", 140000L),
+//                Tuple3.of("b", "baroque", 140000L),
+//                Tuple3.of("a", "abdicate", 210000L),
+//                Tuple3.of("b", "barren", 210000L)
+//                )
+//                .assignTimestampsAndWatermarks(
+//                        WatermarkStrategy
+//                                .<Tuple3<String, String, Long>>forBoundedOutOfOrderness(Duration.ofMillis(0))
+//                                .withTimestampAssigner((event, timestamp) -> event.f2));
+        DataStream<Tuple3<String, String, Long>> stream = env.addSource(new FixedInputGenerator()); // Source with
+        // actual time gaps
         DataStream<Tuple2<String, Long>> result = stream
                 .keyBy(value -> value.f0)
                 .process(new CountWithTimeoutProcessFunction());
@@ -81,7 +83,7 @@ public class CountWithTimeoutBatch {
             state.update(current);
 
             // set timer for 60s later
-            ctx.timerService().registerEventTimeTimer(current.lastModified + 60000);
+            ctx.timerService().registerEventTimeTimer(current.lastModified + 1000);
         }
 
         @Override
@@ -94,7 +96,7 @@ public class CountWithTimeoutBatch {
 
             // check if this timer is latest
             System.out.println("timestamp = " + timestamp + ", result.lastModified = " + result.lastModified);
-            if (timestamp == result.lastModified + 60000) {
+            if (timestamp == result.lastModified + 1000) {
                 // emit the state on timeout
                 System.out.println("result.key = " + result.key + ", result.count = " + result.count);
                 out.collect(new Tuple2<String, Long>(result.key, result.count));
@@ -104,6 +106,43 @@ public class CountWithTimeoutBatch {
 }
 
 /*
+Can simulate timeout using a source generated from batch:
+Sleeping for 0
+Sleeping for 500
+element = (a,abase,0), timestamp = 0
+Sleeping for 2000
+element = (b,bard,500), timestamp = 500
+Sleeping for 500
+element = (a,abate,2500), timestamp = 2500
+timestamp = 1000, result.lastModified = 2500 // Why the 1000 timer fires after the 2500 timestamp?
+timestamp = 1500, result.lastModified = 500
+result.key = b, result.count = 1
+2> (b,1)
+Sleeping for 2000
+element = (b,barrage,3000), timestamp = 3000
+Sleeping for 500
+timestamp = 4000, result.lastModified = 3000
+result.key = b, result.count = 2
+element = (a,abbreviate,5000), timestamp = 5000
+2> (b,2)
+timestamp = 3500, result.lastModified = 5000
+Sleeping for 2000
+element = (b,baroque,5500), timestamp = 5500
+Sleeping for 500
+timestamp = 6500, result.lastModified = 5500
+result.key = b, result.count = 3
+2> (b,3)
+element = (a,abdicate,7500), timestamp = 7500
+timestamp = 6000, result.lastModified = 7500
+element = (b,barren,8000), timestamp = 8000
+timestamp = 9000, result.lastModified = 8000
+timestamp = 8500, result.lastModified = 7500
+result.key = b, result.count = 4
+result.key = a, result.count = 4
+2> (b,4)
+6> (a,4) // Why only the final count is printed for key a?
+
+Event time doesn't work that way with env.fromElements():
 element = (a,abase,0), timestamp = 0
 element = (b,bard,0), timestamp = 0
 element = (a,abate,70000), timestamp = 70000
@@ -112,8 +151,8 @@ element = (a,abbreviate,140000), timestamp = 140000
 element = (b,baroque,140000), timestamp = 140000
 element = (a,abdicate,210000), timestamp = 210000
 element = (b,barren,210000), timestamp = 210000
-timestamp = 60000, result.lastModified = 210000 // firing of 60s timer set at 0, why result.lastModified is alrd 210000?
-timestamp = 60000, result.lastModified = 210000
+timestamp = 1000, result.lastModified = 210000 // firing of 60s timer set at 0, why result.lastModified is alrd 210000?
+timestamp = 1000, result.lastModified = 210000
 timestamp = 130000, result.lastModified = 210000 // firing of 60s timer set at 70000
 timestamp = 130000, result.lastModified = 210000
 timestamp = 200000, result.lastModified = 210000 // firing of 60s timer set at 140000
---
+++ examples/src/main/java/processFunction/FixedInputGenerator.java
@@ -0,0 +1,45 @@
+package processFunction;
+
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.watermark.Watermark;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Random;
+
+public class FixedInputGenerator implements SourceFunction<Tuple3<String, String, Long>> {
+    private volatile boolean running = true;
+    private Tuple3<String, String, Long>[] data = new Tuple3[] {
+            Tuple3.of("a", "abase", 0L),
+            Tuple3.of("b", "bard", 500L),
+            Tuple3.of("a", "abate", 2500L),
+            Tuple3.of("b", "barrage", 3000L),
+            Tuple3.of("a", "abbreviate", 5000L),
+            Tuple3.of("b", "baroque", 5500L),
+            Tuple3.of("a", "abdicate", 7500L),
+            Tuple3.of("b", "barren", 8000L)
+    };
+
+    @Override
+    public void run(SourceContext<Tuple3<String, String, Long>> ctx) throws Exception {
+        int i = 0;
+        Tuple3<String, String, Long> prev = Tuple3.of("", "", 0L);
+        while (running && i < data.length) {
+            Tuple3<String, String, Long> element = data[i];
+            i++;
+
+            System.out.println("Sleeping for " + (element.f2 - prev.f2));
+            Thread.sleep(element.f2 - prev.f2);
+            prev = element;
+
+            ctx.collectWithTimestamp(element, element.f2);
+            ctx.emitWatermark(new Watermark(element.f2));
+        }
+    }
+
+    @Override
+    public void cancel() {
+        running = false;
+    }
+}
