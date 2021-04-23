10228a24e1ee1c20c9ba61e2841cf5b4921572c7
largecats <linfanxiaolinda@outlook.com>
2021-04-23 11:04:09 +0800

    updated

--- examples/src/main/java/processFunction/CountWithTimeoutBatch.java
+++ examples/src/main/java/processFunction/CountWithTimeoutBatch.java
@@ -16,25 +16,31 @@ import org.apache.flink.util.Collector;
 
 import java.time.Duration;
 
+import common.utils.FixedInputGenerator;
+
 public class CountWithTimeoutBatch {
+
+    public static Tuple3<String, String, Long>[] input = new Tuple3[]{
+            Tuple3.of("a", "abase", 0L),
+            Tuple3.of("b", "bard", 500L),
+            Tuple3.of("a", "abate", 2500L),
+            Tuple3.of("b", "barrage", 3000L),
+            Tuple3.of("a", "abbreviate", 5000L),
+            Tuple3.of("b", "baroque", 5500L),
+            Tuple3.of("a", "abdicate", 7500L),
+            Tuple3.of("b", "barren", 8000L)
+    };
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-//        DataStream<Tuple3<String, String, Long>> stream = env.fromElements( // can't simulate timeout
-//                Tuple3.of("a", "abase", 0L),
-//                Tuple3.of("b", "bard", 0L),
-//                Tuple3.of("a", "abate", 70000L),
-//                Tuple3.of("b", "barrage", 70000L),
-//                Tuple3.of("a", "abbreviate", 140000L),
-//                Tuple3.of("b", "baroque", 140000L),
-//                Tuple3.of("a", "abdicate", 210000L),
-//                Tuple3.of("b", "barren", 210000L)
-//                )
+//        DataStream<Tuple3<String, String, Long>> stream = env.fromElements(input) // can't simulate timeout
 //                .assignTimestampsAndWatermarks(
 //                        WatermarkStrategy
 //                                .<Tuple3<String, String, Long>>forBoundedOutOfOrderness(Duration.ofMillis(0))
 //                                .withTimestampAssigner((event, timestamp) -> event.f2));
-        DataStream<Tuple3<String, String, Long>> stream = env.addSource(new FixedInputGenerator()); // Source with
+        DataStream<Tuple3<String, String, Long>> stream = env.addSource(new FixedInputGenerator().getGenerator(input));
+        // Source
+        // with
         // actual time gaps
         DataStream<Tuple2<String, Long>> result = stream
                 .keyBy(value -> value.f0)
@@ -107,60 +113,52 @@ public class CountWithTimeoutBatch {
 
 /*
 Can simulate timeout using a source generated from batch:
-Sleeping for 0
-Sleeping for 500
-element = (a,abase,0), timestamp = 0
-Sleeping for 2000
 element = (b,bard,500), timestamp = 500
-Sleeping for 500
-element = (a,abate,2500), timestamp = 2500
-timestamp = 1000, result.lastModified = 2500 // Why the 1000 timer fires after the 2500 timestamp?
-timestamp = 1500, result.lastModified = 500
-result.key = b, result.count = 1
+element = (a,abase,0), timestamp = 0
+element = (a,abate,2500), timestamp = 2500 // event time timer will only fire after watermark has passed the timer's fire time; so the following timers are only fired after the 2500ms event arrives
+timestamp = 1500, result.lastModified = 500 // timer set for key b at 500ms for 1500ms
+timestamp = 1000, result.lastModified = 2500 // timer set for key a at 0ms for 1000ms
+result.key = b, result.count = 1 // since the 1000ms timer is not the latest, it won't trigger output by the logic
 2> (b,1)
-Sleeping for 2000
 element = (b,barrage,3000), timestamp = 3000
-Sleeping for 500
 timestamp = 4000, result.lastModified = 3000
 result.key = b, result.count = 2
-element = (a,abbreviate,5000), timestamp = 5000
 2> (b,2)
-timestamp = 3500, result.lastModified = 5000
-Sleeping for 2000
 element = (b,baroque,5500), timestamp = 5500
-Sleeping for 500
 timestamp = 6500, result.lastModified = 5500
 result.key = b, result.count = 3
 2> (b,3)
-element = (a,abdicate,7500), timestamp = 7500
-timestamp = 6000, result.lastModified = 7500
 element = (b,barren,8000), timestamp = 8000
 timestamp = 9000, result.lastModified = 8000
-timestamp = 8500, result.lastModified = 7500
 result.key = b, result.count = 4
-result.key = a, result.count = 4
 2> (b,4)
-6> (a,4) // Why only the final count is printed for key a?
-
-Event time doesn't work that way with env.fromElements():
-element = (a,abase,0), timestamp = 0
-element = (b,bard,0), timestamp = 0
-element = (a,abate,70000), timestamp = 70000
-element = (b,barrage,70000), timestamp = 70000
-element = (a,abbreviate,140000), timestamp = 140000
-element = (b,baroque,140000), timestamp = 140000
-element = (a,abdicate,210000), timestamp = 210000
-element = (b,barren,210000), timestamp = 210000
-timestamp = 1000, result.lastModified = 210000 // firing of 60s timer set at 0, why result.lastModified is alrd 210000?
-timestamp = 1000, result.lastModified = 210000
-timestamp = 130000, result.lastModified = 210000 // firing of 60s timer set at 70000
-timestamp = 130000, result.lastModified = 210000
-timestamp = 200000, result.lastModified = 210000 // firing of 60s timer set at 140000
-timestamp = 200000, result.lastModified = 210000
-timestamp = 270000, result.lastModified = 210000 // firing of 60s timer set at 210000
-timestamp = 270000, result.lastModified = 210000
+element = (a,abbreviate,5000), timestamp = 5000
+timestamp = 3500, result.lastModified = 5000
+element = (a,abdicate,7500), timestamp = 7500
+timestamp = 6000, result.lastModified = 7500
+timestamp = 8500, result.lastModified = 7500
 result.key = a, result.count = 4
-result.key = b, result.count = 4
-2> (b,4) // Why only one result is emitted?
 6> (a,4)
+
+Event time doesn't work that way with bounded input:
+element = (b,bard,1250), timestamp = 1250
+element = (a,abase,0), timestamp = 0
+element = (a,abate,2500), timestamp = 2500
+element = (b,barrage,3750), timestamp = 3750
+element = (a,abbreviate,5000), timestamp = 5000
+element = (b,baroque,6250), timestamp = 6250
+element = (a,abdicate,7500), timestamp = 7500
+element = (b,barren,8750), timestamp = 8750
+timestamp = 1000, result.lastModified = 7500 // firing of timer set at 0ms
+timestamp = 3500, result.lastModified = 7500 // firing of timer set at 2500ms
+timestamp = 6000, result.lastModified = 7500
+timestamp = 8500, result.lastModified = 7500
+timestamp = 2250, result.lastModified = 8750
+timestamp = 4750, result.lastModified = 8750
+timestamp = 7250, result.lastModified = 8750
+timestamp = 9750, result.lastModified = 8750
+result.key = b, result.count = 4
+result.key = a, result.count = 4
+6> (a,4)
+2> (b,4)
  */
